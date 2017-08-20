b18ef64ae87810f96b263584089fc49762a21896
dydaev <dydaev@gmail.com>
2017-08-20 23:10:54 +0300

    rewrite form, magic work, note added)

--- notes-app/client/src/js/components/notes-editor.jsx
+++ notes-app/client/src/js/components/notes-editor.jsx
@@ -1,13 +1,56 @@
 import React from 'react';
 
-import NoteForm from './create-form.jsx';
+const NoteEditor = ({ onNoteAdd, title='', text='', color="#FFFFFF" }) => {
+	let titleInput = '';//must be equal id tag for working method onChangeInput
+	let colorInput = '';
+	let textArea = '';
+	
+	const onChangeInput = e => {
+		const element = eval(e.target.id)
+		element.value = element.value
+	}
 
-const NoteEditor = props => {
-		return (
-			<article>
-				<h1>Notes editor</h1>
-				<NoteForm />
-			</article>
+	const handleSubmit = () => {
+		console.log("push to state. {", titleInput.value + "-" + colorInput.value + "-" + textArea.value ,"}")
+		onNoteAdd({
+			title: titleInput.value,
+			color: colorInput.value,
+			text: textArea.value
+		})
+	}
+
+	return (
+		<div>
+			<input
+				id="titleInput"
+				onChange={onChangeInput}
+				ref={input => titleInput = input} 
+				type="text" 
+				name="title" 
+				placeholder="Title" 
+				size="30"
+			/>
+			<input
+				id="colorInput"
+				onChange={onChangeInput}
+				ref={input => colorInput = input} 
+				type="text" 
+				name="color" 
+				placeholder="color" 
+				size="10"
+			/>
+			<textarea
+				id="textArea"
+				onChange={onChangeInput}
+				ref={area => textArea = area} 
+				cols="30" 
+				name="text" 
+				rows="3" 
+				placeholder="text">
+			</textarea>
+			
+			<input type="submit" onClick={handleSubmit} value="Sent"/>
+		</div>
 )}
 
 export default NoteEditor;
