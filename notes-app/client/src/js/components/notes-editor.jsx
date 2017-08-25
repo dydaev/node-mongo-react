3c1c3765de18684151c02c558d4e70b5b489718a
dydaev <dydaev@gmail.com>
2017-08-25 23:29:51 +0300

    note-edit added editing deleting buttons

--- notes-app/client/src/js/components/notes-editor.jsx
+++ notes-app/client/src/js/components/notes-editor.jsx
@@ -3,8 +3,17 @@ import React, { PureComponent } from 'react';
 import Paper from 'material-ui/Paper';
 import { ChromePicker } from 'react-color';
 import TextField from 'material-ui/TextField';
+import IconButton from 'material-ui/IconButton';
 import RaisedButton from 'material-ui/RaisedButton';
 
+import FloatingActionButton from 'material-ui/FloatingActionButton';
+import ContentCheck from 'material-ui/svg-icons/navigation/check';
+import ContentClose from 'material-ui/svg-icons/navigation/close';
+import ActionEdit from 'material-ui/svg-icons/editor/mode-edit';
+import ActionDelete from 'material-ui/svg-icons/action/delete';
+import ActionHome from 'material-ui/svg-icons/action/home';
+import ColorPNG from 'material-ui/svg-icons/image/brush';
+// import ColorPNG from 'material-ui/svg-icons/image/color-lens';
 
 class NoteEditor extends PureComponent
 {
@@ -12,9 +21,10 @@ class NoteEditor extends PureComponent
 		super(props);
 		this.state = {
 			displayColorPicker: false,
-			title: props.title,
-			text: props.text,
-			color: props.color || '#FFFFFF'
+			title: props.title || '',
+			text: props.text || '',
+			color: props.color || '#FFFFFF',
+			isChanged: false
 		}
 	}
 
@@ -25,22 +35,44 @@ class NoteEditor extends PureComponent
     	this.setState({ displayColorPicker: false })
   	}
 	handleChangeColor(newColor) {
-		this.setState({ color: newColor.hex });
+		this.setState({ 
+			color: newColor.hex,
+			isChanged: true 
+		});
 	}
-
-	handleSubmit() {
-		//console.log("push to state. {", titleInput.value + "-" + colorInput.value + "-" + textArea.value ,"}")
+	handleCahgeTextField(e) {
+		this.setState({ 
+			[e.target.id]: e.target.value,
+			isChanged: true
+		 })
+	}
+	handleCancle() {
+		this.setState({
+			color: this.props.color || '#FFFFFF',
+			text: this.props.text,
+			title: this.props.title,
+			isChanged: false
+		})
+	}
+	handleChanging() {
+		this.setState({ isChanged: true })
+	}
+	handleSubmit(e) {
+		this.setState({ isChanged: false })
 /*		onNoteAdd({
-			title: document.querySelector("#titleInput").value,
-			color: document.querySelector("#").value,
-			text: document.querySelector("#textArea").value
+			title: this.state.title,
+			color: this.state.color,
+			text: this.state.text
 		})*/
-		alert("Saving... title:" + document.querySelector("#titleInput").value )
+		alert("Saving... title: " + this.state.title + " & text: " + this.state.text )
 	}
 	render() {
 		const buttonStile = {
+			width: 60,
+    		height: 60,
 			marginRight: 10,
-			marginTop: 1
+			padding: 10,
+			marginTop: -2
 		}
 		const textItemStyle = {
 			width: 200,
@@ -64,6 +96,54 @@ class NoteEditor extends PureComponent
 	      bottom: '0px',
 	      left: '0px',
 	    }
+    	const colorPicker = this.state.displayColorPicker
+		? (<div style={ popover }>
+          		<div style={ cover } onClick={ this.handleClosePicker.bind(this) }/>
+          		<ChromePicker  onChange={this.handleChangeColor.bind(this)} color={this.state.color} />
+        	</div>)
+		: null
+	    const newNoteButtons = (
+	    		<div>
+		    		<IconButton 
+		    			label="Color" 
+		    			style={ buttonStile } 
+		    			onClick={ this.handleClickPicker.bind(this) }>
+						<ColorPNG />
+				    </IconButton>
+			        { colorPicker }
+					<IconButton 
+						label="Save" 
+						style={ buttonStile } 
+						onClick={this.handleSubmit.bind(this)}>
+						<ContentCheck />
+					</IconButton>
+					<IconButton 
+						label="Cancle" 
+						onClick={this.handleCancle.bind(this)}>
+						<ContentClose />
+					</IconButton>
+				</div>
+	    )
+
+	    const noteButtons = (
+	    	<div>
+	    		<IconButton 
+					label="Edit"
+					onClick={this.handleChanging.bind(this)}>
+					<ActionEdit />
+				</IconButton>
+	    		<IconButton 
+					label="Delete">
+					<ActionDelete />
+				</IconButton>
+	    	</div>
+	    );
+
+
+	    const Buttons = this.state.isChanged
+	    	? newNoteButtons
+	    	: noteButtons
+
 		return (
 			<div className="note-thumbnail">
 			<Paper 
@@ -72,13 +152,17 @@ class NoteEditor extends PureComponent
 				rounded={false} 
 			>	
 				<TextField
-					id="titleInput"
+					id="title"
+					disabled={this.state.isChanged ? false : true}
+					onChange={this.handleCahgeTextField.bind(this)}
 					style={textItemStyle}
 	      			hintText="Title"
 	      			value={this.state.title}
 	    		/>
 	    		<TextField
-					id="textArea"
+					id="text"
+					disabled={this.state.isChanged ? false : true}
+					onChange={this.handleCahgeTextField.bind(this)}
 					style={textItemStyle}
 			     	hintText="Text"
 			     	multiLine={true}
@@ -86,16 +170,8 @@ class NoteEditor extends PureComponent
 			     	rowsMax={4}
 			     	value={this.state.text}
 			    />
-				<RaisedButton style={ buttonStile } label="Color" onClick={ this.handleClickPicker.bind(this) }/>
-			    
-		        { this.state.displayColorPicker ? 
-		        	<div style={ popover }>
-		          		<div style={ cover } onClick={ this.handleClosePicker.bind(this) }/>
-		          		<ChromePicker  onChange={this.handleChangeColor.bind(this)} color={this.state.color} />
-		        	</div> 
-		        	: null }
 
-				<RaisedButton label="Save" onClick={this.handleSubmit.bind(this)}/>
+			    {Buttons}
 			</Paper>			
 			</div>
 		)}
